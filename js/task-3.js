972ec8f4b0d81c3ab9946e95d8a6ff5e760958ca
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2023-12-28 18:19:59 +0100

    v2

--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,12 @@
-class StringBuilder {
-  #value;
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-  constructor(initialValue) {
-    this.#value = initialValue;
+nameInput.addEventListener('input', (event) => {
+  const inputValue = event.target.value.trim();
+
+  if (inputValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = inputValue;
   }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+});
